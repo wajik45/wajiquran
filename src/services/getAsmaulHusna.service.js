1d7f0c0f6051210c38c4e9ca842ba9bf6b0b30c5
wajik45 <haikal2589780@gmail.com>
2023-08-09 21:15:52 +0700

    ok

--- src/services/getAsmaulHusna.service.js
+++ src/services/getAsmaulHusna.service.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-const BASEURL = import.meta.env.VITE_BASE_URL_ASMAUL_HUSNA;
 
 export const getAsmaulHusna = async () => {
-  return await axios.get(BASEURL);
+  return await axios.get("src/data/asmaulHusna.json");
 };
